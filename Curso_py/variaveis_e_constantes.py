720ab2c4ed2b8691243ec20cd364eaccb1ecf64b
gleicesantosdev <116775112+gleicesantosdev@users.noreply.github.com>
2024-08-30 09:45:34 -0300

    variaveis e constantes

---
+++ Curso_py/variaveis_e_constantes.py
@@ -0,0 +1,13 @@
+#o padrão de nomes deve ser snake case - exemplo: snake_case
+#escolha de nomes sugestivos 
+#nomes de constantes devem ser maiusculas
+#variaveis = valores mutáveis 
+#constantes = armazena memória, os valores não se alteram 
+
+nome = "Ketolle"  #variaveis
+idade = 20 #variaveis
+print(nome, idade) #saída
+
+limite_saque_diario = 1000 #snake case
+
+BRAZILIAN_STATES = ["SP", "PE", "MG", "PA"] # constantes tb seguem snake case
