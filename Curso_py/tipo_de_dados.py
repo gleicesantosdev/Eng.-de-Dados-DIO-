b856c72eb575f11333659be999a3665033b3ea9e
gleicesantosdev <116775112+gleicesantosdev@users.noreply.github.com>
2024-08-30 09:02:01 -0300

    exemplos de tipos de dados

---
+++ Curso_py/tipo_de_dados.py
@@ -0,0 +1,40 @@
+#tipo inteiro - int() 
+"""tipo int = números inteiros, representados pelo tipo int. 
+ Um inteiro, ou int, é um número sem uma parte decimal, podendo ser
+ positivo ou negativo."""
+#exemplos
+print(5 + 3)
+print(10 - 2)
+print(4 * 2)
+print(16 / 2) 
+
+#tipo string - str("")
+
+""" Uma string é uma sequência de caracteres e é definida em Python por caracteres entre aspas simples (‘…’) ou duplas (“…”)."""
+#exemplo str
+print("hello" + "world") #concatena
+print("hello world" [0:5]) #fatiamento, resulta em hello
+print("World" in "Hello World") #resulta em true - busca 
+
+#tipo float 
+""" se trata de representar números com casas decimais, 
+indo além da capacidade dos inteiros. Por exemplo, o número 3.14 
+é um float, sendo a representação de Python para o valor de pi numérico."""
+#exemplo float
+print(1.5 + 1 + 0.5) #resulta 3.0
+
+#tipo bool
+"""representa dois valores: True (verdadeiro) ou False (falso).
+números diferentes de zero são tratados como True, enquanto o zero é
+tratado como False. Listas, strings ou qualquer coleção vazia também 
+são consideradas False quando avaliadas em um contexto Booleano.
+permitindo o controle de fluxo através de instruções condicionais 
+como if e else. """
+
+#exemplo bool
+print(true) #tipo bool
+print(false) #tipo bool
+
+
+
+
